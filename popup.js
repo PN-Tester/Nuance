7189601eb203d4ad7442117f3973d3166d3e3a05
PN <95763550+PN-Tester@users.noreply.github.com>
2023-07-27 18:02:20 -0400

    Update popup.js
    added listeners for the buttons to trigger the right functions in content.js

--- popup.js
+++ popup.js
@@ -7,6 +7,27 @@ function replaceSelectedText(style) {
   });
 }
 
+// Function to handle the button click for inserting diacritical mark above
+function insertDiacriticalMarkAbove() {
+  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+    chrome.tabs.sendMessage(tabs[0].id, { action: 'replaceSelectedText', style: 'diacritical' });
+  });
+}
+
+// Function to handle the button click for inserting diacritical mark above
+function insertSTIRT() {
+  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+    chrome.tabs.sendMessage(tabs[0].id, { action: 'replaceSelectedText', style: 'STIRT' });
+  });
+}
+
+// Function to handle the button click for inserting diacritical mark above
+function insertRightLeftMark() {
+  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+    chrome.tabs.sendMessage(tabs[0].id, { action: 'replaceSelectedText', style: 'RightLeft' });
+  });
+}
+
 // Wait for the DOM to be ready before adding the event listeners
 document.addEventListener('DOMContentLoaded', function () {
   document.getElementById('boldButton').addEventListener('click', function () {
@@ -15,5 +36,18 @@ document.addEventListener('DOMContentLoaded', function () {
 
   document.getElementById('italicButton').addEventListener('click', function () {
     replaceSelectedText('italic');
+  });
+
+  // Add event listener for the diacritical button
+  document.getElementById('diacriticalButton').addEventListener('click', function () {
+    insertDiacriticalMarkAbove();
+  });
+  // Add event listener for the stirt button
+  document.getElementById('stirtButton').addEventListener('click', function () {
+    insertSTIRT();
+  });
+  // Add event listener for the rightLeft button
+  document.getElementById('rightLeftButton').addEventListener('click', function () {
+    insertRightLeftMark();
   });
 });
